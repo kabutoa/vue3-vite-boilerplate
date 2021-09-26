1e47b3a66b7dfe9a2563cbb38187d54ef62ebeab
kabutoa <yinyun957@163.com>
2021-09-26 19:26:59 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>hello world</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+
+<style>
+#app {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
