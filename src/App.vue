34349a8560322f69e215fb72b90f20a1f0f898cb
kabutoa <yinyun957@163.com>
2021-09-28 14:39:36 +0800

    router jump

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,66 @@
 <template>
+  <div class="nav">
+    <button
+      v-for="button in buttonConfig"
+      :key="button.id"
+      @click="onJumpRouter(button.type)"
+    >
+      {{ button.text }}
+    </button>
+  </div>
   <router-view></router-view>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+
+export interface buttonConfigIF {
+  id: number
+  text: string
+  type: string
+}
+
+const buttonConfig: Array<buttonConfigIF> = [
+  {
+    id: 1,
+    text: 'home',
+    type: 'home'
+  },
+  {
+    id: 2,
+    text: 'about',
+    type: 'about'
+  }
+]
 
 export default defineComponent({
-  setup() {}
+  setup() {
+    const router = useRouter()
+
+    const onJumpRouter = (type: string) => {
+      router.push({ name: type })
+    }
+
+    return {
+      buttonConfig,
+      onJumpRouter
+    }
+  }
 })
 </script>
+
+<style lang="scss" scoped>
+.nav {
+  display: flex;
+  width: 100%;
+
+  > button {
+    width: px2vw(120);
+    height: px2vh(48);
+    background-color: #409eff;
+    border-radius: 4px;
+    color: #fff;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,10 +5,12 @@ const router = createRouter({
   routes: [
     {
       path: '',
+      name: 'home',
       component: () => import('@/views/home/index.vue')
     },
     {
       path: '/about',
+      name: 'about',
       component: () => import('@/views/about/index.vue')
     }
   ]
