becdd07c401278bef8d9db36add660b8813770c3
kabutoa <yinyun957@163.com>
2024-01-11 12:06:30 +0800

    feat: add element-ui

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,1 +1,11 @@
-<template>home page</template>
+<template>
+  <div class="home-view">
+    <el-button @click="onMessage">test message</el-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const onMessage = () => {
+  ElMessage('this is a message.')
+}
+</script>
