f52478cd33700000dfc6adaa2aef9ffedf498772
kabutoa <yinyun957@163.com>
2024-01-11 12:15:36 +0800

    feat: add api

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="home-view">
-    <el-button @click="onMessage">test message</el-button>
+  <div class="home">
+    <div class="home-title">home page</div>
+    <div class="home-message">
+      <el-button @click="onMessage">test message</el-button>
+    </div>
+    <div class="home-request">
+      <el-button @click="onRequest">test request</el-button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { getUserInfo } from '@/api'
+
 const onMessage = () => {
-  ElMessage('this is a message.')
+  ElMessage('this is a message')
+}
+
+const onRequest = async () => {
+  const userInfo = await getUserInfo()
+  console.log(userInfo)
 }
 </script>
