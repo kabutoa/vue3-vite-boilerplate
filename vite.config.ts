aaa77c4c903a29c043410f3fc9df4f37d6def054
kabutoa <yinyun957@gmail.com>
2024-01-12 11:02:51 +0800

    feat: add vite-eslint-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import eslint from 'vite-plugin-eslint'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -12,6 +13,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    eslint(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
       eslintrc: {
